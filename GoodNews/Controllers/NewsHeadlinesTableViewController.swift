f50b4f2e53e678dbec0de7217b94c0d7ea987619
erdem ozgur <erdmzgr@gmail.com>
2020-04-03 13:42:09 +0300

    Populating UITableView with Dummy Data
    Populating UITableView with Dummy Data

--- GoodNews/Controllers/NewsHeadlinesTableViewController.swift
+++ GoodNews/Controllers/NewsHeadlinesTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class NewsHeadlinesTableViewController: UITableViewController {
     
+    let news = [["title": "This is the title", "description": "This is the descriptionn"], ["title": "This is the title", "description": "This is the descriptionn This is the descriptionn This is the descriptionn This is the descriptionn "] ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +22,32 @@ class NewsHeadlinesTableViewController: UITableViewController {
     
     fileprivate func setupUI() {
                 
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 600
+        
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.news.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsHeadlineTableViewCell", for: indexPath) as? NewsHeadlineTableViewCell else {
+            fatalError("NewsHeadlineTableViewCell is not found")
+        }
+        cell.titleLabel.text = self.news[indexPath.row]["title"]
+        cell.descriptionLabel.text = self.news[indexPath.row]["description"]
+        
+        return cell
+        
+        
         
     }
     
